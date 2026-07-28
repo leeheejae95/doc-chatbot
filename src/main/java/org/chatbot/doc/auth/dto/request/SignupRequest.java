6fbd9c2ccc96b1c88139fa55a6b2373cfa8315f6
leeheejae95 <leeheejae55@naver.com>
2026-07-28 12:50:20 +0900

    test: AuthService, ChatService, JwtUtil 단위 테스트 추가
    - AuthServiceTest: 회원가입 성공/이메일 중복, 로그인 성공/없는 유저/비밀번호 불일치 (5케이스)
    - ChatServiceTest: 답변 성공, conversationId 유지, 빈, null, 공백 질문 예외 (5케이스)
    - JwtUtilTest: 토큰 생성/이메일 추출/유효, 변조, 만료 검증 (5케이스)
    - 요청 DTO에 @AllArgsConstructor 추가 (테스트 객체 생성 목적)

--- src/main/java/org/chatbot/doc/auth/dto/request/SignupRequest.java
+++ src/main/java/org/chatbot/doc/auth/dto/request/SignupRequest.java
@@ -1,10 +1,12 @@
 package org.chatbot.doc.auth.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class SignupRequest {
     private String email;
     private String password;
--- src/main/java/org/chatbot/doc/chat/dto/request/ChatRequest.java
+++ src/main/java/org/chatbot/doc/chat/dto/request/ChatRequest.java
@@ -1,10 +1,12 @@
 package org.chatbot.doc.chat.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class ChatRequest {
     private String question;
 
